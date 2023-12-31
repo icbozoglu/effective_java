8bd8db6ec240cb49eba5563b7b4641464f48283e
İzzet <email>
2023-12-31 11:43:14 +0300

    builder 5

--- consider.builder.with.many.constructor.parameters/src/consider/builder/with/many/constructor/parameters/Test.java
+++ consider.builder.with.many.constructor.parameters/src/consider/builder/with/many/constructor/parameters/Test.java
@@ -7,8 +7,9 @@ import consider.builder.with.many.constructor.parameters.model.Person;
 public class Test {
 
 	public static void main(String[] args) {
-		
-		Person person = new Person.Builder("Ali","Veli",true).birthDate(LocalDate.now()).build();
+		// The builder’s setter methods return the builder itself so that invocations
+		// can be chained, resulting in a fluent API.
+		Person person = new Person.Builder("Ali", "Veli", true).birthDate(LocalDate.now()).build();
 		System.out.println(person);
 
 	}
